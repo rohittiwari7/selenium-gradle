1eb013e8c17cac98fcaeb7e0814fea937a2deaff
rohit <rohit.tiwari7@gmail.com>
2020-12-20 01:38:08 +0530

    time adjustment

--- src/test/java/Google.java
+++ src/test/java/Google.java
@@ -13,7 +13,7 @@ public class Google
     driver.get("https://www.google.com");
 
     driver.findElement(By.name("q")).sendKeys("IntelliJ IDEA");
-        Thread.sleep(2000);
+
         driver.findElement(By.cssSelector("div.hsuHs")).click();
     Thread.sleep(2000);
     driver.quit();
