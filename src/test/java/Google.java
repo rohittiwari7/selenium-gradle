2630b8998b808318fec3e9c106d99d9f8d459e7f
rohitiwari <rohitiwari@admins-MacBook-Pro.local>
2020-12-19 20:42:08 +0530

    first commit

---
+++ src/test/java/Google.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+public class Google
+{
+    @Test
+    public void login() throws InterruptedException {
+    System.setProperty("webdriver.chrome.driver","ChromeDriver");
+    WebDriver driver=new ChromeDriver();
+    driver.get("https://www.google.com");
+
+    driver.findElement(By.name("q")).sendKeys("IntelliJ IDEA");
+    Thread.sleep(3000);
+    driver.quit();
+    }
+}
